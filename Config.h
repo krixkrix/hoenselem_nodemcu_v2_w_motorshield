3e24cbe88b9d00a5bbac733a22ad491d5d529132
kn <kristian.noergaard@xaar3d.com>
2019-10-13 17:36:26 +0200

    A working version

--- Config.h
+++ Config.h
@@ -20,9 +20,9 @@ class Config {
 
 public:
   int open_hour = 7;
-  int open_minutes = 20;
-  int close_hour = 19;
-  int close_minutes = 00;
+  int open_minutes = 30;
+  int close_hour = 18;
+  int close_minutes = 45;
   int poll_interval_minutes = 10;
   int force_open = 0;
   int force_close = 0;
